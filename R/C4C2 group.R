895f735b52b508d85a49d46c6dc289810dfe42fd
Karene <ceciliakarene123@gmail.com>
2022-05-01 16:18:03 -0400

    re-upload C4C2 group due to an issue

---
+++ R/C4C2 group.R
@@ -0,0 +1,33 @@
+#'@title 
+#'Create a C4C2 sgroupr_sgrp object
+#'
+#'@description The length of this group should be 7
+#'@param x, an integer vector
+#'@example
+#'C4C2(0:7)
+#'
+#'@export
+new_C4C2 <- function(x = integer()) {
+  vec_assert(x, integer())
+  x[x > 7L | x < 0L] <- NA_integer_
+  new_sgrp(x, group = "C4C2")
+}
+ 
+
+C4C2 <- function(x = integer()) {
+  x <- vec_cast(x, integer())
+  new_C4C2(x)
+}
+ 
+
+is_C4C2 <- function(x) {
+  result <- FALSE
+  stopifnot(is_sgrp(x))
+  if(attributes(x)$group == "C4C2") {
+    return(!result)
+  } else return(result)
+}
+
+
+
+
