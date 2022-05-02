031239cf4436b33509463ac32c6c28f039674656
Karene <ceciliakarene123@gmail.com>
2022-05-01 20:12:04 -0400

    imported vtcrs package

---
+++ R/coercion.R
@@ -0,0 +1,19 @@
+#'@title
+#' Coercion file created
+#'
+#' @export
+vec_ptype_abbr.sgroupr_sgrp <- function(x, ...) {
+  "sgrp"
+}
+
+# coercion
+#' @export
+vec_ptype2.sgroupr_sgrp.sgroupr_sgrp<- function(x, y, ...) {
+  new_sgrp(group = group(y))
+}
+
+#' @export
+vec_ptype2.sgroupr_sgrp.integer <- function(x, y, ...) new_sgrp(group = group(x))
+
+#' @export
+vec_ptype2.integer.sgroupr_sgrp <- function(x, y, ...) new_sgrp(group = group(y))
