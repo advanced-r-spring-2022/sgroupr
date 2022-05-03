a5e583c73c4c7e940442255867bbe569ecc54572
Chi-Lin Lin <cl6076a@student.american.edu>
2022-05-02 23:39:06 -0400

    fix error

--- R/tables.R
+++ R/tables.R
@@ -257,7 +257,7 @@ table <- function(x) {
                         12L,13L,14L,15L,0L,1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,11L,
                         13L,14L,15L,12L,1L,2L,3L,0L,5L,6L,7L,4L,9L,10L,11L,8L,
                         14L,15L,12L,13L,2L,3L,0L,1L,6L,7L,4L,5L,10L,11L,8L,9L,
-                        15L,12L,13L,14L,3L,0L,1L,2L,7L,4L,5L,6L,11L,8L,9L,10L,),
+                        15L,12L,13L,14L,3L,0L,1L,2L,7L,4L,5L,6L,11L,8L,9L,10L),
                       nrow = 16,ncol = 16)
       rownames(table) <- 0:15
       colnames(table) <- 0:15
